2d5665e3adc4ac2bd548c31f284dd12613961c79
Jobayer75 <jobayerjoyaddar@gmail.com>
2023-09-30 22:02:04 +0800

    Js

---
+++ case-convert.js
@@ -0,0 +1,9 @@
+const anthem = 'I love you';
+const search = 'crypto';
+
+const userInput = 'lol';
+const savedUserName = 'lol';
+
+if (userInput.toLowerCase() == savedUserName.toLocaleLowerCase()) {
+  console.log('user here');
+}
---
+++ quiz-02.js
@@ -0,0 +1,11 @@
+{
+  /* <button onclick="myFunction()">Click me</button>  */
+}
+document.getElementById('order').addEventListener('click', function () {});
+
+{
+  /* <p class=”nav”>I am Jobayer 1</p>
+<p class=”nav”>I am Jobayer 2</p>
+
+var p = document.getElementsByClassName('nav')[0]; */
+}
