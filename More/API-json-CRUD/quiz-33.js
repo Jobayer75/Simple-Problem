35626168ed8a7f6563bab609387de038987471f5
Jobayer75 <jobayerjoyaddar@gmail.com>
2023-09-30 22:22:18 +0800

    up

---
+++ More/API-json-CRUD/quiz-33.js
@@ -0,0 +1,8 @@
+// const stuInfo = JSON.stringify({ name: 'times', roll: 3 });
+// console.log(stuInfo.name);
+
+// const premikas = { name: 'shakib al hasan', cars: { brand: 'toyota' } };
+// console.log(({ band } = amar.cars));
+
+const info = { data: [{ language: 'Javascript', library: 'React' }] };
+console.log(info.data[0].library);
